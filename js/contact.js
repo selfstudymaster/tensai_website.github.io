b3a3da1ffe3a54af0fa49c388a08fdeeac38f3f2
selfstudymaster <webselfmaster@gmail.com>
2021-09-16 19:16:31 +0900

    contactフォーム実装 googleフォーム連携 自動返信設定

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+        url: "https://docs.google.com/forms/u/2/d/e/1FAIpQLSeDTp8zTdfjqJdd1RAqWNSiZCHrS6IJtWuiy80DBgJcX7eyGQ/formResponse",
+        data: formData,
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+        0: function () {
+            $(".end-message").slideDown();
+            $(".submit-btn").fadeOut();
+            window.location.href = "thanks.html";
+        },
+        200: function () {
+            $(".false-message").slideDown();
+        }
+        }
+    });
+    event.preventDefault();
+    });
+
+});
